86ebe1ed16cb7e9586e48164e25ab173d9ff7943
Pedro <pedhenriquecm@gmail.com>
2025-04-16 16:41:41 -0300

    First Post Comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from './components/Header.jsx';
-import { Post } from './Post.jsx';
+import { Post } from './components/Post.jsx';
+import { Sidebar } from './components/Sidebar.jsx';
+
+import styles from './App.module.css';
 
 import './global.css'; // escoped CSS
 
@@ -7,14 +10,20 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Diego Fernandes" 
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, saepe! Quia, eaque deleniti obcaecati, sunt vero quibusdam delectus iure laborum sapiente totam doloribus autem reprehenderit suscipit temporibus corporis ullam nemo!"
-      />
-      <Post
-        author="Mayk Brito"
-        content="Novo Post"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor."
+          />
+          <Post
+            author="Pedro"
+            content="Teste"
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,37 @@
+import styles from './Post.module.css';
+
+export function Post(){
+    return(
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/pedrosiuberaba.png" alt="" />
+                    <div className={styles.authorInfo}>
+                        <strong>Pedro Henrique</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='16 de Abril às 14:05' dateTime='2025-04-16 14:05:00'>Publicado há 1h</time>
+            </header>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p><a href="">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href=''>#nlw </a>{' '}
+                    <a href=''>#rocketseat </a>{' '}
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea placeholder='Deixe um comentário' />
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+        </article>
+    );
+}
