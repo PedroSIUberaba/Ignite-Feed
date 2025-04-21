1f90e13f2f4feff5ae4ced3e1fea20a0d4575c4d
Pedro <pedhenriquecm@gmail.com>
2025-04-21 00:14:02 -0300

    Comment Component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <img src="https://github.com/pedrosiuberaba.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Pedro Henrique</strong>
+                            <time title='16 de Abril às 14:05' dateTime='2025-04-16 14:05:00'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+
+        </div>
+    )
+}
